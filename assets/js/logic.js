6e49f3bdcc64775f24114996e812eed291e2167f
neyneyalldayday <drfrankenstein123@gmail.com>
2024-02-14 12:01:00 -0600

    saved object to local storage

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -4,6 +4,23 @@ const submitButton = document.querySelector("#submit-button")
 
 blogForm.addEventListener("submit", (event) =>{
   event.preventDefault()
+  const blogTitle = document.querySelector("#blog-title").value.trim()
+  const blogContent = document.querySelector("#blog-content").value.trim()
+  const userName = document.querySelector("#username").value.trim();
+  
+  const blogObject = JSON.parse(localStorage.getItem("blogObject")) || [];
+
+  const blogPayload = {
+    blogTitle: blogTitle,
+    blogContent : blogContent,
+    userName : userName
+  }
+
+  blogObject.push(blogPayload)
+
+  localStorage.setItem("blogObject", JSON.stringify(blogObject))
   console.log("click")
+
+  window.location.href = "blog.html";
 })
 
