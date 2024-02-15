1812cbcfc3c1c9765c27dfc2e526292032508585
Rene Trevino <drfrankenstein123@gmail.com>
2024-02-14 20:05:26 -0600

    blogs work

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,1 +1,21 @@
-console.log("yo we here")
+const blogContainer = document.querySelector(".blog-container")
+let blogCard = ""
+const blogData = JSON.parse(localStorage.getItem("blogObject")) || []
+ 
+blogData.forEach((blog) => {
+    blogCard += `
+    <div class="blog-card">
+        <h2 class="render-title">${blog.blogTitle}</h2>
+        <p class="render-content">${blog.blogContent}</p>
+        <h2 class="render-user">${blog.userName}</h2>
+    </div>
+    `;
+
+    blogContainer.innerHTML = blogCard;
+})
+
+
+document.querySelector(".back-home").addEventListener("click", (event) => {
+    event.preventDefault();
+    window.location.href = "index.html";
+})
